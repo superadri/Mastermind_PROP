42cc7ccd90444a8d709290442f8bba6cdcaa6ec7
superadri <superadri@users.noreply.github.com>
2017-10-08 21:00:44 +0200

    Add files via upload

---
+++ MasterMind.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mastermind;
+
+/**
+ *
+ * @author Sony
+ */
+public class MasterMind {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Algoritme a = new Algoritme(4,4,false);
+        a.start_c();
+    }
+    
+}
