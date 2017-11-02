dfe418d2e121bcdfeb497e1bda1109430a05e462
SirDrope <pedro.llanos@est.fib.upc.edu>
2017-11-02 18:28:23 +0100

    fixed the TimeClass, it's ready!! Second it's the unit you can to work

--- Time.java
+++ Time.java
@@ -6,38 +6,46 @@ import java.util.*;
 public class Time {
 
 		// Attribute
-    double iniTime, finalTime;
+    int iniTime, finalTime;
+	Calendar c;
 
 		// Constructor
 	public Time() {
-		this.iniTime = 0.0;
-		this.finalTime = 0.0;
+		this.iniTime = 0;
+		this.finalTime = 0;
 	}
 
 		// Getter && Setter
-    public double GetTime() {
-		return (finalTime-iniTime);
+    public int GetTime() {
+		return (finalTime - iniTime);
 	}
     // public void SetTime(double newTime) {  iniTime; }
 
 		// Method
 	public void StartTime(){
 		System.out.println("Start Crono");
-		this.iniTime = 1.0;
+		c = Calendar.getInstance();
+		System.out.println( "Minute: " + c.get(Calendar.MINUTE) + ", Second: " + c.get(Calendar.SECOND) );
+		this.iniTime = ( c.get(Calendar.MINUTE)*60 + c.get(Calendar.SECOND) );
 	}
 
 	public void StopTime(){
 		System.out.println("Stop Crono");
-		this.finalTime = 4.0;
+		c = Calendar.getInstance();
+		System.out.println( "Minute: " + c.get(Calendar.MINUTE) + ", Second: " + c.get(Calendar.SECOND) );
+		this.finalTime = ( c.get(Calendar.MINUTE)*60 + c.get(Calendar.SECOND) );
 	}
 
+		// Test Method
+	/*
 	public static void main(String[] args) {
 		Time t = new Time();
 		t.StartTime();
+		try { Thread.sleep(10000); }
+		catch(InterruptedException ex) { Thread.currentThread().interrupt(); }
 		t.StopTime();
-		System.out.println(t.GetTime());
 
-	    Calendar c = Calendar.getInstance();
-	    System.out.println("Time: "+ c.HOUR +" "+c.MINUTE+" "+c.SECOND+" "+c.MILLISECOND);
+		System.out.println(t.GetTime());
 	}
+	*/
 }
